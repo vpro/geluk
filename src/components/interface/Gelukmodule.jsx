692009f247c0939a440f3ef8fb59ce3ef77c0857
evanzummeren <erikvanzummeren@gmail.com>
2016-03-03 17:30:32 +0100

    verzekeringh

--- src/components/interface/Gelukmodule.jsx
+++ src/components/interface/Gelukmodule.jsx
@@ -5,43 +5,42 @@ import InputRange from 'react-input-range';
 class Gelukmodule extends React.Component{
   constructor(props){
     super(props);
-
     this.submitHappiness = this.submitHappiness.bind(this);
     this.setRef = this.setRef.bind(this);
   }
 
   setRef(ref){
-    this.specificHappiness = ref;
+    this.refHappiness = ref;
   }
 
   submitHappiness(){
-    var happinessValue = this.specificHappiness.value;
-    // this.specificHappiness = '';
-    console.log(happinessValue);
-    console.log('component', this);
-    this.props.setHappy(happinessValue, "q_2");
+    var currentQuestion = this.props.happinessQuestion,
+        givenHappiness = Number(this.refHappiness.value);
+
+    this.props.setHappy(givenHappiness, currentQuestion);
   }
+
   render() {
-    console.log(this.props.currentValue)
 		return (
 			<div className="gelukmodule">
-				<p>Gelukmodule</p>
-        {this.props.currentValue}
+				<p>{this.props.questionDescription}</p>
         <input 
           type="range"
-          min={0}
-          max={10}
-          value={this.props.currentValue}
+          min={this.props.lowestScale}
+          max={this.props.highestScale}
+          value={this.props.happinessValue}
           onChange={this.submitHappiness} 
           ref={this.setRef}
         />
+        {this.props.happinessValue}
 			</div>
 		)
 	}
 }
 
 Gelukmodule.propTypes = {
-  currentValue: React.PropTypes.number.isRequired,
+  happinessValue: React.PropTypes.number.isRequired,
+  happinessQuestion: React.PropTypes.string.isRequired,
   setHappy: React.PropTypes.func.isRequired
 }
 
